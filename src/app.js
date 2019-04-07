f07158b700980f2fe5ba20ae512ffab887eaa411
Magda Sidor <magda_si@wp.pl>
2019-04-07 22:41:15 +0200

    project init

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import script from './script';
+import './main.scss';
+
+script();
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/assets/test.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CopyWebpackPlugin([
+      {
+          from: './src/assets/images',
+          to: 'images'
+      } 
+  ]),
+  ],
+  watch: true,
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+            isProduction
+            ? MiniCssExtractPlugin.loader
+            : { loader: 'style-loader', options: { sourceMap: true } },
+              { loader: 'css-loader', options: { sourceMap: isProduction } },
+              { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+              { loader: 'sass-loader', options: { sourceMap: isProduction } },
+        ]
+    }
+    ]
+  }
+};
